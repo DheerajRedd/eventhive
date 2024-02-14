c1187c4c28fa86d1ec0f007414a29cd9c9191e65
dheerajredd <dheeraj5555@yahoo.com>
2024-02-13 19:55:03 -0500

    pagination and filter

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,67 +1,72 @@
+import CategoryFilter from "@/components/shared/CategoryFilter";
 import Collection from "@/components/shared/Collection";
+import Search from "@/components/shared/Search";
 import { Button } from "@/components/ui/button";
 import { getAllEvents } from "@/lib/actions/event.actions";
+import { SearchParamProps } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 
-export default async function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || "";
+  const category = (searchParams?.category as string) || "";
+
   const events = await getAllEvents({
-    query: "",
-    category: "",
-    page: 1,
+    query: searchText,
+    category,
+    page,
     limit: 6,
   });
-  console.log(events);
+
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
-        <div className="wrapper grid-cols-1 gap-5 md:grid-cols-2">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
             <h1 className="h1-bold">
-              Create, Connect, Celebrate: Your Event, Your Way, Right Away!
+              Host, Connect, Celebrate: Your Events, Our Platform!
             </h1>
             <p className="p-regular-20 md:p-regular-24">
-              Expert <span className="font-bold">Guidance</span> , Endless{" "}
-              <span className="font-bold">Possibilities</span>. Book and learn
-              helpful tips from 765+ mentors in world class companies with our
-              global community.
+              Book and learn helpful tips from 3,168+ mentors in world-class
+              companies with our global community.
             </p>
-            <Button size="lg" asChild className="button w-full sm:w-fit my-4">
-              <Link className="" href="#events">
-                Dive into Possibility
-              </Link>
+            <Button size="lg" asChild className="button w-full sm:w-fit">
+              <Link href="#events">Explore Now</Link>
             </Button>
           </div>
-          <div className="flex justify-center items-center h-full rounded-lg ">
-            <Image
-              src="/assets/images/sectionimg.png"
-              alt="hero"
-              width={800}
-              height={500}
-              className="object-contain rounded-lg max-h-[70vh] object-contain object-center 2xl:max-h-[50vh] "
-            />
-          </div>
+
+          <Image
+            src="/assets/images/hero.png"
+            alt="hero"
+            width={1000}
+            height={1000}
+            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+          />
         </div>
       </section>
+
       <section
         id="events"
         className="wrapper my-8 flex flex-col gap-8 md:gap-12"
       >
         <h2 className="h2-bold">
-          Where Thousands of Events,
-          <br /> Place Their Trust
+          Trust by <br /> Thousands of Events
         </h2>
-        <div className="flex 2-full flex-col gap-5 md:flex-row">
-          Search Category Filter
+
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          <Search />
+          <CategoryFilter />
         </div>
+
         <Collection
-          data={[]}
+          data={events?.data}
           emptyTitle="No Events Found"
-          emptyStateSubtext="Come Back later"
+          emptyStateSubtext="Come back later"
           collectionType="All_Events"
           limit={6}
-          page={1}
-          totalPages={2}
+          page={page}
+          totalPages={events?.totalPages}
         />
       </section>
     </>
