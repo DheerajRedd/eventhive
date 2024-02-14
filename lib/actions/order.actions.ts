a0c91e3f4a44dcb7277c38b340e308a6fd88b8c7
dheerajredd <dheeraj5555@yahoo.com>
2024-02-13 21:46:27 -0500

    Uploadthing Changes

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -9,11 +9,11 @@ import {
 } from "@/types";
 import { redirect } from "next/navigation";
 import { handleError } from "../utils";
-import { connectToDatabase } from "@/lib/database";
-import Order from "@/lib/database/models/order.model";
-import Event from "@/lib/database/models/event.model";
+import { connectToDatabase } from "../database";
+import Order from "../database/models/order.model";
+import Event from "../database/models/event.model";
 import { ObjectId } from "mongodb";
-import User from "@/lib/database/models/user.model";
+import User from "../database/models/user.model";
 
 export const checkoutOrder = async (order: CheckoutOrderParams) => {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
